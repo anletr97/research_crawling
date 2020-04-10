c9cdaa1b48c66b971570ef0b99b6076f1c4577cb
An Lê <anletr97@gmail.com>
2020-04-10 10:57:01 +0700

    Crawl product name

---
+++ crawler/spiders/crawler.py
@@ -0,0 +1,19 @@
+from scrapy import Spider
+from scrapy.selector import Selector
+from crawler.items import ProductItem
+
+class CrawlerSpider(Spider):
+    name = "crawler"
+    allowed_domains=["www.tiki.vn"]
+    start_urls=["https://tiki.vn/dien-thoai-iphone-7-plus-128gb-hang-nhap-khau-chinh-hang-p269707.html"]
+
+    def parse(self, res):
+        product = Selector(res).xpath('//div[@class="item-box"]')
+
+        item = ProductItem()
+
+        item['pName'] = product.xpath('h1[@class="item-name"]/span/text()').extract_first()
+        item['pPrice'] = product.xpath('div[@class="item-price"]/text()').extract_first()
+
+        yield item
+        
